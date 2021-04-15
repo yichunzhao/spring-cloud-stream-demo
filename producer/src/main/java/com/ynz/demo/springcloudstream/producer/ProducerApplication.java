7719ea0d0793c1fb9722504c9e95b1959d0aecec
yichunzhao <zhaoyichun@hotmail.com>
2021-04-15 23:18:14 +0200

    Initial commit

---
+++ producer/src/main/java/com/ynz/demo/springcloudstream/producer/ProducerApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.springcloudstream.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+}
---
+++ producer/src/test/java/com/ynz/demo/springcloudstream/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.springcloudstream.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
